35b5b8b1b0f232334ba471ad25e3531c09c9fbad
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:55:34 +0530

    Refactor add to cart button and adjust wait time
    Renamed ADD_TO_CART to ADD_BTN and updated wait time.

--- selenium-tests/pages/product_page.py
+++ selenium-tests/pages/product_page.py
@@ -3,18 +3,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class ProductPage:
+
     TITLE = (By.CSS_SELECTOR, "div.product-name h1")
-    ADD_TO_CART = (By.ID, "add-to-cart-button-4")
+    ADD_BTN = (By.ID, "add-to-cart-button-4")
 
     def __init__(self, driver):
         self.driver = driver
 
     def get_title(self):
-        return WebDriverWait(self.driver, 15).until(
+        return WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located(self.TITLE)
         ).text
-
-    def add_to_cart(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.ADD_TO_CART)
-        ).click()
