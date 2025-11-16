f838bd9b599b302506318adf851b6d159745185e
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:11:21 +0530

    Update register_page.py

--- selenium-tests/pages/register_page.py
+++ selenium-tests/pages/register_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -10,9 +10,8 @@ class RegisterPage:
 
     def open(self):
         self.driver.get(self.url)
-        self._close_cookie_banner()
 
-    def _close_cookie_banner(self):
+        # Close cookie popup if shown
         try:
             cookie_btn = WebDriverWait(self.driver, 3).until(
                 EC.element_to_be_clickable((By.ID, "eu-cookie-ok"))
@@ -22,7 +21,22 @@ class RegisterPage:
             pass
 
     def register(self, first_name, last_name, email, password):
-        self._select_gender()
+
+        # ---- REQUIRED: Select gender ----
+        try:
+            self.driver.find_element(By.ID, "gender-male").click()
+        except:
+            pass
+
+        # ---- REQUIRED: Fill date of birth ----
+        try:
+            Select(self.driver.find_element(By.NAME, "DateOfBirthDay")).select_by_value("1")
+            Select(self.driver.find_element(By.NAME, "DateOfBirthMonth")).select_by_value("1")
+            Select(self.driver.find_element(By.NAME, "DateOfBirthYear")).select_by_value("1990")
+        except:
+            pass
+
+        # ---- Fill remaining fields ----
         self.driver.find_element(By.ID, "FirstName").send_keys(first_name)
         self.driver.find_element(By.ID, "LastName").send_keys(last_name)
         self.driver.find_element(By.ID, "Email").send_keys(email)
@@ -30,24 +44,19 @@ class RegisterPage:
         self.driver.find_element(By.ID, "ConfirmPassword").send_keys(password)
 
         register_btn = self.driver.find_element(By.ID, "register-button")
+
+        # Scroll to button
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", register_btn)
 
+        # Normal click → fallback to JS click
         try:
             register_btn.click()
         except:
             self.driver.execute_script("arguments[0].click();", register_btn)
 
-        msg = WebDriverWait(self.driver, 10).until(
+        # Wait for success message
+        msg = WebDriverWait(self.driver, 15).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "result"))
         ).text
 
         return msg
-
-    def _select_gender(self):
-        try:
-            gender = WebDriverWait(self.driver, 5).until(
-                EC.element_to_be_clickable((By.ID, "gender-male"))
-            )
-            gender.click()
-        except:
-            pass
