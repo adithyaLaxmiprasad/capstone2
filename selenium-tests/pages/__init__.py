f24a2928be4f2dbfc845241b3f3cd16e609ed3ee
Adithya L <1by22is400@bmsit.in>
2025-11-16 12:59:18 +0530

    Update __init__.py

--- selenium-tests/pages/__init__.py
+++ selenium-tests/pages/__init__.py
@@ -3,4 +3,4 @@ from .search_results_page import SearchResultsPage
 from .product_page import ProductPage
 from .cart_page import CartPage
 from .register_page import RegisterPage
-from .checkout_page import CheckoutPage
+# from .checkout_page import CheckoutPage
