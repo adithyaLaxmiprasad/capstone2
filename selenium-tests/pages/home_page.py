1fea827b6c388187dc8b5b056b9b220db1828598
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:54:54 +0530

    Refactor search button variable and update search method

--- selenium-tests/pages/home_page.py
+++ selenium-tests/pages/home_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class HomePage:
     SEARCH_INPUT = (By.ID, "small-searchterms")
-    SEARCH_BUTTON = (By.CSS_SELECTOR, "button.search-box-button")
+    SEARCH_BTN = (By.CSS_SELECTOR, "button.search-box-button")
 
     def __init__(self, driver, base_url):
         self.driver = driver
@@ -14,10 +14,9 @@ class HomePage:
         self.driver.get(self.url)
 
     def search(self, text):
-        WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable(self.SEARCH_INPUT)
-        ).send_keys(text)
-
         WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.SEARCH_BUTTON)
-        ).click()
+            EC.element_to_be_clickable(self.SEARCH_INPUT)
+        ).clear()
+
+        self.driver.find_element(*self.SEARCH_INPUT).send_keys(text)
+        self.driver.find_element(*self.SEARCH_BTN).click()
