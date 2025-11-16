0072dd5ce8bc992e6933d19d2920f9e96cd3fa5e
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:47:13 +0530

    Refactor search results page to use PRODUCT_TITLES

--- selenium-tests/pages/search_results_page.py
+++ selenium-tests/pages/search_results_page.py
@@ -3,18 +3,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class SearchResultsPage:
-    FIRST_PRODUCT = (By.CSS_SELECTOR, "h2.product-title a")
+    PRODUCT_TITLES = (By.CSS_SELECTOR, "h2.product-title a")
 
     def __init__(self, driver):
         self.driver = driver
 
     def first_product_title(self):
-        el = WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located(self.FIRST_PRODUCT)
-        )
-        return el.text.strip()
+        return WebDriverWait(self.driver, 15).until(
+            EC.visibility_of_all_elements_located(self.PRODUCT_TITLES)
+        )[0].text
 
     def open_first_product(self):
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.FIRST_PRODUCT)
-        ).click()
+        WebDriverWait(self.driver, 15).until(
+            EC.element_to_be_clickable(self.PRODUCT_TITLES)
+        )[0].click()
