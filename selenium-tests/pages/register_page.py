635fc73d4554033e0747da17b04989e83bab9c52
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:54:35 +0530

    Refactor register method and clean up code
    Refactor register method to use clearer parameter names and remove cookie popup handling.

--- selenium-tests/pages/register_page.py
+++ selenium-tests/pages/register_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class RegisterPage:
+
     def __init__(self, driver, base_url):
         self.driver = driver
         self.url = f"{base_url}register"
@@ -10,35 +11,16 @@ class RegisterPage:
     def open(self):
         self.driver.get(self.url)
 
-        # Close cookie popup if appears
-        try:
-            btn = WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.ID, "eu-cookie-ok"))
-            )
-            self.driver.execute_script("arguments[0].click();", btn)
-        except:
-            pass
-
-    def register(self, first, last, email, password):
-
-        # Fill required fields
-        self.driver.find_element(By.ID, "gender-male").click()
-
-        Select(self.driver.find_element(By.NAME, "DateOfBirthDay")).select_by_value("1")
-        Select(self.driver.find_element(By.NAME, "DateOfBirthMonth")).select_by_value("1")
-        Select(self.driver.find_element(By.NAME, "DateOfBirthYear")).select_by_value("1990")
-
-        self.driver.find_element(By.ID, "FirstName").send_keys(first)
-        self.driver.find_element(By.ID, "LastName").send_keys(last)
+    def register(self, first_name, last_name, email, password):
+        self.driver.find_element(By.ID, "FirstName").send_keys(first_name)
+        self.driver.find_element(By.ID, "LastName").send_keys(last_name)
         self.driver.find_element(By.ID, "Email").send_keys(email)
         self.driver.find_element(By.ID, "Password").send_keys(password)
         self.driver.find_element(By.ID, "ConfirmPassword").send_keys(password)
 
-        btn = self.driver.find_element(By.ID, "register-button")
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", btn)
-        self.driver.execute_script("arguments[0].click();", btn)
+        self.driver.find_element(By.ID, "register-button").click()
 
-        msg = WebDriverWait(self.driver, 15).until(
+        msg = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "result"))
         ).text
 
