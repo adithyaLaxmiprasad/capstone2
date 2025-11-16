a8495c405ba2539386e9763003168fdc8007e4e8
Adithya L <1by22is400@bmsit.in>
2025-11-16 13:55:17 +0530

    Reduce wait time and change element visibility checks

--- selenium-tests/pages/search_results_page.py
+++ selenium-tests/pages/search_results_page.py
@@ -3,17 +3,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class SearchResultsPage:
+
     PRODUCT_TITLES = (By.CSS_SELECTOR, "h2.product-title a")
 
     def __init__(self, driver):
         self.driver = driver
 
     def first_product_title(self):
-        return WebDriverWait(self.driver, 15).until(
-            EC.visibility_of_all_elements_located(self.PRODUCT_TITLES)
-        )[0].text
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.PRODUCT_TITLES)
+        ).text
 
     def open_first_product(self):
-        WebDriverWait(self.driver, 15).until(
-            EC.element_to_be_clickable(self.PRODUCT_TITLES)
-        )[0].click()
+        self.driver.find_elements(*self.PRODUCT_TITLES)[0].click()
